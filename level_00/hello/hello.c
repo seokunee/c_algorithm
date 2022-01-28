eb4cddc2ef4fc2e6a2be98c3688cbb9131e4c31a
seokunee <xocoolree@gmail.com>
2022-01-29 03:12:44 +0900

    쉘00 ex01 ex02

---
+++ level_00/hello/hello.c
@@ -0,0 +1,7 @@
+#include<unistd.h>
+
+int	main()
+{
+	write(1,"hello world\n", 12);
+	return (0);
+}
--- level_02/inter/inter.c
+++ level_02/inter/inter.c
@@ -1,7 +1,39 @@
-#inclue<unistd.h>
+#include<unistd.h>
 
 int	main(int ac, char **av)
 {
-	
+	int	i;
+	int	j;
+	int	k;
 
+	i = 0;
+	j = 0;
+	k = 0;
+	if (ac == 3)
+	{
+		while(av[1][i])
+		{
+			while(av[2][j])
+			{
+				if(av[1][i] == av[2][j] && av[1][i] > 32)
+				{
+					write(1, &av[1][i], 1);
+					break;
+				}
+				j++;
+			}
+			j = 0;
+			k = i + 1;
+			while(av[1][k])
+			{
+				if (av[1][i] == av[1][k])
+					av[1][k] = '1';
+				k++;
+			}
+			k = 0;
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
 }
